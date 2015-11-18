f6a38f5e7620b6cd4d565e08feaeedd60579943d
Damian Arturo Garces Paez <guners86@hotmail.com>
2015-11-18 13:42:09 +0000

    Se corrige error de token CSRF

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::ProductsController < ApplicationController
 
+  # Se evita el error (Cant verify CSRF token authenticity)
+  # que se genera por el helper form_for y que a su vez genera el token CSRF
+  skip_before_filter :verify_authenticity_token
+
   def index
     render json: Product.all
   end
@@ -7,7 +11,7 @@ class Api::V1::ProductsController < ApplicationController
   def create
     product = Product.new(product_params)
     if product.save
-      render json: product, status: 201, location: [:api, product]
+      render json: product, status: 201
     else
       render json: { errors: product.errors }, status: 422
     end
